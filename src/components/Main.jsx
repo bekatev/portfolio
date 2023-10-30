6d576bf5f3fce0187d171f8d57860a01c92ff6a8
bekatev <74668742+bekatev@users.noreply.github.com>
2023-10-30 21:40:16 +0400

    add: deploy workflow

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,8 +13,8 @@ import animationData from "../assets/Animation1.json";
 const Main = () => {
   return (
     <div id="main" className="relative">
-      <div className="w-full h-screen object-cover bg-bckg" />
-      <div className="max-w-[400px] md:max-w-[800px] xl:max-w-[1100px] max-h-[600px] m-auto h-full w-full bg-primary flex flex-col justify-center items-center absolute top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 rounded-xl shadow-xl">
+      <div className="w-11/12 h-screen object-cover bg-bckg" />
+      <div className="max-w-[350px] md:max-w-[800px] xl:max-w-[1100px] max-h-[600px] m-auto h-full w-full bg-primary flex flex-col justify-center items-center absolute top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 rounded-xl shadow-xl">
         <h1 className="text-2xl md:text-4xl xl:text-5xl text-white text-center">
           Hello, I'm
         </h1>
@@ -33,9 +33,9 @@ const Main = () => {
           />
         </h2>
         <Lottie animationData={animationData} className="w-96" />
-        <div className="flex flex-row justify-between pt-6 max-w-[500px] w-full">
+        <div className="flex flex-row justify-between pt-6 max-w-[500px] w-full px-4">
           <p>
-            <b>Mobile:</b> +995 591242873
+            <b>Mobile:</b> 591242873
           </p>
           <p>
             <b>Email:</b> bekatevd@gmail.com
